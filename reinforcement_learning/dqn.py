43928d6dbac9c2792f857a3ac5ca927d270b7629
fmars <fmarsf@gmail.com>
2023-11-22 15:26:40 -0800

    refactor

--- reinforcement_learning/dqn.py
+++ reinforcement_learning/dqn.py
@@ -1,7 +1,7 @@
 # Reference
 #   Huggingface Deep RL Course https://huggingface.co/learn/deep-rl-course/unit3/deep-q-algorithm
 #   vwxyzjn implementation https://github.com/vwxyzjn/cleanrl/blob/master/cleanrl/dqn_atari.py
-# 
+#
 # TODO:
 #   0. refactor
 #   1. wandb
@@ -38,11 +38,14 @@ from stable_baselines3.common.atari_wrappers import (
 )
 import torch
 
+assert torch.cuda.is_available()
+d = torch.device("cuda")
+
 
 @dataclass
 class Config:
     n_episode = 10000
-    record_every_n = 5000
+    record_every_n = 3000
     n_step = 10000
     train_start_at_n = 100
     train_every_n = 8
@@ -98,7 +101,6 @@ class QNetwork(torch.nn.Module):
         return self.network(x / 255.0)
 
 
-
 class ReplayBuffer:
     @dataclass
     class In:
@@ -141,53 +143,58 @@ class ReplayBuffer:
         return ReplayBuffer.BatchedOut(obs, action, next_obs, reward, done)
 
 
-assert torch.cuda.is_available()
-d = torch.device("cuda")
+def train(env, m, target_m, opt, rb, cfg):
+    for episode in tqdm(range(cfg.n_episode)):
+        obs, info = env.reset()
+        for step in range(cfg.n_step):
+            eps = cfg.min_eps + (cfg.max_eps - cfg.min_eps) * np.exp(
+                -cfg.decay_rate * episode
+            )
+            rand = np.random.uniform(0.0, 1.0)
+            if rand >= eps:
+                x = torch.Tensor(obs).unsqueeze(0).to(d)
+                logits = m(x)
+                action = torch.argmax(logits).cpu().item()
+            else:
+                action = env.action_space.sample()
+            next_obs, reward, termination, truncation, info = env.step(action)
 
-cfg = Config()
-env = make_env(cfg)
-m = QNetwork(env.action_space.n).to(d)
-opt = torch.optim.Adam(m.parameters(), lr=cfg.lr)
-target_m = QNetwork(env.action_space.n).to(d)
-target_m.load_state_dict(m.state_dict())
-rb = ReplayBuffer(cfg.replay_buffer_size)
+            if termination or truncation:
+                break
 
-for episode in tqdm(range(cfg.n_episode)):
-    obs, info = env.reset()
-    for step in range(cfg.n_step):
-        eps = cfg.min_eps + (cfg.max_eps - cfg.min_eps) * np.exp(
-            -cfg.decay_rate * episode
-        )
-        rand = np.random.uniform(0.0, 1.0)
-        if rand >= eps:
-            x = torch.Tensor(obs).unsqueeze(0).to(d)
-            logits = m(x)
-            action = torch.argmax(logits).cpu().numpy()
-        else:
-            action = env.action_space.sample()
-        next_obs, reward, termination, truncation, info = env.step(action)
+            rb.add(obs, action, next_obs, reward, termination, truncation, info)
 
-        if termination or truncation:
-            print(f'episode {episode} ends at {step=}')
-            break
-
-        rb.add(obs, action, next_obs, reward, termination, truncation, info)
-
-        if step > cfg.train_start_at_n:
-            if step % cfg.train_every_n == 0:
-                data = rb.sample(cfg.batch_size)
-                with torch.no_grad():
-                    max_q, _ = target_m(data.next_obs.to(d)).max(dim=1)
-                    y = data.reward.to(d) + cfg.discount * max_q * (1.0 - data.done.to(d))
-                pred = m(data.obs.to(d)).gather(dim=1, index=data.action.view(-1, 1).to(d))
-                loss = torch.nn.functional.mse_loss(pred, y)
-                print(f'episode {episode} {loss=}')
-
-                opt.zero_grad()
-                loss.backward()
-                opt.step()
-            if step % cfg.sync_every_n == 0:
-                target_m.load_state_dict(m.state_dict())
+            if step > cfg.train_start_at_n:
+                if step % cfg.train_every_n == 0:
+                    data = rb.sample(cfg.batch_size)
+                    with torch.no_grad():
+                        max_q, _ = target_m(data.next_obs.to(d)).max(dim=1)
+                        y = data.reward.to(d) + cfg.discount * max_q * (
+                            1.0 - data.done.to(d)
+                        )
+                    pred = m(data.obs.to(d)).gather(
+                        dim=1, index=data.action.view(-1, 1).to(d)
+                    ).squeeze(1)
+                    loss = torch.nn.functional.mse_loss(pred, y)
+                    opt.zero_grad()
+                    loss.backward()
+                    opt.step()
+                if step % cfg.sync_every_n == 0:
+                    target_m.load_state_dict(m.state_dict())
+        print(f'At {episode} {loss=}')
 
 
-env.close()
+def main():
+    cfg = Config()
+    env = make_env(cfg)
+    m = QNetwork(env.action_space.n).to(d)
+    opt = torch.optim.Adam(m.parameters(), lr=cfg.lr)
+    target_m = QNetwork(env.action_space.n).to(d)
+    target_m.load_state_dict(m.state_dict())
+    rb = ReplayBuffer(cfg.replay_buffer_size)
+    train(env, m, target_m, opt, rb, cfg)
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
