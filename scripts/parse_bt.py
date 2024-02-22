084a089410147d4d7d2f240a39b6b0fb52327a6a
Jiacheng Feng <fjc@openai.com>
2024-02-22 13:57:46 -0800

    parse bt

--- scripts/parse_bt.py
+++ scripts/parse_bt.py
@@ -4,13 +4,21 @@
 # cp parse_bt.py /usr/local/bin/parse_bt
 # echo "...your bt..." | parse_bt
 import sys
-
+import argparse
 
 def main():
+    argparser = argparse.ArgumentParser(description="Parse a backtrace")
+    argparser.add_argument("trim", type=int, default=0) 
+    args = argparser.parse_args()
+
     traceback_input = sys.stdin.read()
     traceback_lines = traceback_input.split("\n")
     for line in traceback_lines:
         if not line.startswith("->"):
+            if args.trim > 0:
+                parts = line.split("/")
+                parts = parts[args.trim:]
+                line = "/".join(parts)
             print(line)
 
 
