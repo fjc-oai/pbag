a2ca622eaae8989702b835c1c27202626255835c
Jiacheng Feng <fjc@openai.com>
2024-02-26 00:19:59 -0800

    improve bt parser

--- scripts/parse_backtrace.py
+++ scripts/parse_backtrace.py
@@ -1,25 +1,42 @@
 #!/usr/bin/env python3
 
-# chmod +x parse_bt.py
-# cp parse_bt.py /usr/local/bin/parse_bt
+# chmod +x parse_backtrace.py
+# cp parse_backtrace.py /usr/local/bin/parse_bt
 # echo "...your bt..." | parse_bt
-import sys
 import argparse
+import sys
+
+
+def _process_frame(line, args):
+    if line.startswith("->"):
+        return ""
+    if args.drop_frame:
+        key_works = [
+            "lib/python3",
+        ]
+        for key in key_works:
+            if key in line:
+                return ""
+    if args.trim_prefix:
+        parts = line.split("/")
+        line = "/".join(parts[4:])
+    return line
+
 
 def main():
     argparser = argparse.ArgumentParser(description="Parse a backtrace")
-    argparser.add_argument("trim", type=int, default=0) 
+    argparser.add_argument("--drop-frame", action="store_true", default=True)
+    argparser.add_argument("--trim-prefix", action="store_true", default=True)
     args = argparser.parse_args()
 
     traceback_input = sys.stdin.read()
+    print("\n")
     traceback_lines = traceback_input.split("\n")
     for line in traceback_lines:
-        if not line.startswith("->"):
-            if args.trim > 0:
-                parts = line.split("/")
-                parts = parts[args.trim:]
-                line = "/".join(parts)
+        line = _process_frame(line, args)
+        if line:
             print(line)
+    print("\n")
 
 
 if __name__ == "__main__":
