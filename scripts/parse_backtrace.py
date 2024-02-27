d3fb43a031a9a9bc316113d9d69527b99668aba6
Jiacheng Feng <fjc@openai.com>
2024-02-27 15:33:49 -0800

    parse_bt: support logger format

--- scripts/parse_backtrace.py
+++ scripts/parse_backtrace.py
@@ -23,17 +23,49 @@ def _process_frame(line, args):
     return line
 
 
+def _process_frame_logger_format(line, args):
+    # breakpoint()
+    if not line:
+        return ""
+    pos = line.find(")")
+    assert pos != -1, f"Invalid line format: {line}"
+    line = line[pos + 1 :]
+    line = line.strip()
+    if not line.startswith("File"):
+        return ""
+
+    if args.drop_frame:
+        key_works = [
+            "lib/python3",
+        ]
+        for key in key_works:
+            if key in line:
+                return ""
+
+    line = line[len('File "') :]
+    pos = line.find('"')
+    path = line[:pos]
+    if args.trim_prefix:
+        parts = path.split("/")
+        path = "/".join(parts[4:])
+    return path + line[pos + 1 :]
+
+
 def main():
     argparser = argparse.ArgumentParser(description="Parse a backtrace")
     argparser.add_argument("--drop-frame", action="store_true", default=True)
     argparser.add_argument("--trim-prefix", action="store_true", default=True)
+    argparser.add_argument("--logger-output", action="store_true", default=False)
     args = argparser.parse_args()
 
     traceback_input = sys.stdin.read()
     print("\n")
     traceback_lines = traceback_input.split("\n")
     for line in traceback_lines:
-        line = _process_frame(line, args)
+        if args.logger_output:
+            line = _process_frame_logger_format(line, args)
+        else:
+            line = _process_frame(line, args)
         if line:
             print(line)
     print("\n")
