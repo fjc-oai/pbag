442acb1f0e42674adaf54dcd0b5a104277de3726
Jiacheng Feng <fjc@openai.com>
2024-07-25 16:12:54 -0700

    flash attn: multi-head torch

--- pytorch/triton/flash_attn/torch_impl.py
+++ pytorch/triton/flash_attn/torch_impl.py
@@ -2,68 +2,96 @@ import torch
 
 
 def torch_native(q, k, v):
-    return torch.nn.functional.scaled_dot_product_attention(q, k, v)
+    batch, q_len, n_heads, d_head = q.shape
+    _, kv_len, _, _ = k.shape
+    q = q.permute(0, 2, 1, 3).reshape(batch * n_heads, q_len, d_head)
+    k = k.permute(0, 2, 1, 3).reshape(batch * n_heads, kv_len, d_head)
+    v = v.permute(0, 2, 1, 3).reshape(batch * n_heads, kv_len, d_head)
+    o = torch.nn.functional.scaled_dot_product_attention(q, k, v)
+    o = o.view(batch, n_heads, q_len, d_head).permute(0, 2, 1, 3)
+    return o
 
 
-def _fwd_kernel(q, k, v, o, n_heads, seqlen, d_head, tid_0, tid_1, BLOCK):
-    seq_block_idx = tid_0
-    off_hb = tid_1
-    batch_idx = off_hb // n_heads
-    head_idx = off_hb % n_heads
+def torch_manual(q, k, v):
+    batch, q_len, n_heads, d_head = q.shape
+    _, kv_len, _, _ = k.shape
+    q = q.permute(0, 2, 1, 3).reshape(batch * n_heads, q_len, d_head)
+    k = k.permute(0, 2, 1, 3).reshape(batch * n_heads, kv_len, d_head)
+    v = v.permute(0, 2, 1, 3).reshape(batch * n_heads, kv_len, d_head)
+    s = q @ k.transpose(1, 2) / (d_head**0.5)
+    p = torch.nn.functional.softmax(s, dim=-1)
+    o = p @ v
+    o = o.view(batch, n_heads, q_len, d_head).permute(0, 2, 1, 3)
+    return o
+
+
+def _fwd_kernel(q, k, v, o, tid_0, tid_1, tid_2, Q_BLOCK, KV_BLOCK):
+    batch, q_len, n_heads, d_head = q.shape
+    _, kv_len, _, _ = k.shape
+    batch_idx = tid_0
+    head_idx = tid_1
+    q_block_idx = tid_2
 
     q_block = q[
-        batch_idx, seq_block_idx * BLOCK : (seq_block_idx + 1) * BLOCK, head_idx
-    ]  # [BLOCK, d_head]
+        batch_idx, q_block_idx * Q_BLOCK : (q_block_idx + 1) * Q_BLOCK, head_idx
+    ]  # [Q_BLOCK, d_head]
     acc = None
-    m = torch.full((BLOCK,), float("-inf"))  # [BLOCK]
-    l = torch.full((BLOCK,), float("-inf"))  # [BLOCK]
-    n_blocks = seqlen // BLOCK
-    for j in range(n_blocks):
-        k_block = k[batch_idx, j * BLOCK : (j + 1) * BLOCK, head_idx]  # [BLOCK, d_head]
-        v_block = v[batch_idx, j * BLOCK : (j + 1) * BLOCK, head_idx]  # [BLOCK, d_head]
-        s = q_block @ k_block.T  # [BLOCK, BLOCK]
-        m_j = torch.max(s.max(dim=1).values, m)  # [BLOCK]
+    m = torch.full((Q_BLOCK,), float("-inf"))  # [Q_BLOCK]
+    l = torch.full((Q_BLOCK,), float("-inf"))  # [Q_BLOCK]
+    n_kv_blocks = kv_len // KV_BLOCK
+    for j in range(n_kv_blocks):
+        k_block = k[batch_idx, j * KV_BLOCK : (j + 1) * KV_BLOCK, head_idx]  # [KV_BLOCK, d_head]
+        v_block = v[batch_idx, j * KV_BLOCK : (j + 1) * KV_BLOCK, head_idx]  # [KV_BLOCK, d_head]
+        s = q_block @ k_block.T / (d_head**0.5)  # [Q_BLOCK, KV_BLOCK]
+        m_j = torch.max(s.max(dim=-1).values, m)  # [Q_BLOCK]
         if j == 0:
-            acc = torch.exp(s - m_j) @ v_block
-            l = torch.exp(s - m_j).sum(dim=1)
+            acc = torch.exp(s - m_j[:, None]) @ v_block  # [Q_BLOCK, d_head]
+            l = torch.exp(s - m_j[:, None]).sum(dim=-1)  # [Q_BLOCK]
         else:
-            acc *= torch.exp(m - m_j)[:, None]
-            acc += torch.exp(s - m_j) @ v_block
-            l *= torch.exp(m - m_j)
-            l += torch.exp(s - m_j).sum(dim=1)
+            acc *= torch.exp(m - m_j)[:, None]  # [Q_BLOCK, d_head]
+            acc += torch.exp(s - m_j[:, None]) @ v_block  # [Q_BLOCK, d_head]
+            l *= torch.exp(m - m_j)  # [Q_BLOCK]
+            l += torch.exp(s - m_j[:, None]).sum(dim=-1)  # [Q_BLOCK]
         m = m_j
-    acc /= l[:, None]
-    o[batch_idx, seq_block_idx * BLOCK : (seq_block_idx + 1) * BLOCK, head_idx].copy_(acc)
+    acc /= l[:, None]  # [Q_BLOCK, d_head]
+    o[batch_idx, q_block_idx * Q_BLOCK : (q_block_idx + 1) * Q_BLOCK, head_idx] = acc
     return
 
 
 def flash_attn_torch(q, k, v):
-    batch, seqlen, n_heads, d_head = q.shape
-    assert q.shape == k.shape == v.shape
+    batch, q_len, n_heads, d_head = q.shape
     o = torch.zeros_like(q)
-    BLOCK = 32
-    for tid_0 in range(seqlen // BLOCK):
-        for tid_1 in range(batch * n_heads):
-            print(f"Progress: {tid_0 * batch * n_heads + tid_1}/{seqlen * batch * n_heads // BLOCK}")
-            _fwd_kernel(q, k, v, o, n_heads, seqlen, d_head, tid_0, tid_1, BLOCK)
+    Q_BLOCK = 32
+    KV_BLOCK = 32
+    for tid_0 in range(batch):
+        for tid_1 in range(n_heads):
+            for tid_2 in range(q_len // Q_BLOCK):
+                _fwd_kernel(q, k, v, o, tid_0, tid_1, tid_2, Q_BLOCK, KV_BLOCK)
     return o
 
 
 def main():
-    batch_size = 1
-    seqlen = 128
-    n_heads = 1
+    torch.manual_seed(7)
+    batch_size = 4
+    q_len = 1024
+    kv_len = 1024
+    n_heads = 8
     d_head = 32
-    q = torch.rand(batch_size, seqlen, n_heads, d_head)
-    k = torch.rand(batch_size, seqlen, n_heads, d_head)
-    v = torch.rand(batch_size, seqlen, n_heads, d_head)
+    q = torch.randn(batch_size, q_len, n_heads, d_head)
+    k = torch.randn(batch_size, kv_len, n_heads, d_head)
+    v = torch.randn(batch_size, kv_len, n_heads, d_head)
 
-    ref_out = torch.nn.functional.scaled_dot_product_attention(q, k, v)
+    ref1 = torch_native(q, k, v)
+    ref2 = torch_manual(q, k, v)
     out = flash_attn_torch(q, k, v)
-    diff = torch.abs(out - ref_out)
-    rel = diff / torch.abs(ref_out)
-    print(f"out <> ref_out: {diff.max()}, {rel.max()}")
-    breakpoint()
+    diff = torch.abs(ref1 - ref2)
+    rel = diff / torch.abs(ref2)
+    print(f"ref1 <> ref2: {diff.max()}, {rel.max()}")
+    diff = torch.abs(ref1 - out)
+    rel = diff / torch.abs(out)
+    print(f"ref1 <> out: {diff.max()}, {rel.max()}")
+    assert torch.allclose(ref1, ref2, atol=1e-5)
+    assert torch.allclose(ref1, out, atol=1e-5)
 
 
 if __name__ == "__main__":
--- pytorch/triton/flash_attn/x.py
+++ pytorch/triton/flash_attn/x.py
@@ -24,7 +24,7 @@ def test_dot_attention_order():
 
 def test_accumuate_denominator():
     seqlen = 1024
-    s = torch.rand(seqlen)
+    s = torch.randn(seqlen)
     m = s.max()
     ref_out = torch.exp(s - m).sum()
 
@@ -47,8 +47,8 @@ def test_accumuate_denominator():
 
 def test_accumuate_numerator():
     seqlen = 1024
-    s = torch.rand(seqlen)
-    v = torch.rand(seqlen)
+    s = torch.randn(seqlen)
+    v = torch.randn(seqlen)
     m = s.max()
     rel_out = torch.exp(s - m) @ v
 
@@ -72,9 +72,9 @@ def test_accumuate_numerator():
 
 def test_single_q_number():
     seqlen = 1024
-    q = torch.rand(1)
-    k = torch.rand(seqlen)
-    v = torch.rand(seqlen)
+    q = torch.randn(1)
+    k = torch.randn(seqlen)
+    v = torch.randn(seqlen)
 
     def ref_impl(q, k, v):
         s = q * k
@@ -112,18 +112,22 @@ def test_single_q_number():
 def test_single_q_vector():
     seqlen = 1024
     d_head = 8
-    q = torch.rand(d_head)
-    k = torch.rand(seqlen, d_head)
-    v = torch.rand(seqlen, d_head)
+    q = torch.randn(d_head)
+    k = torch.randn(seqlen, d_head)
+    v = torch.randn(seqlen, d_head)
 
     def ref_impl(q, k, v):
-        s = q @ k.T
+        s = q @ k.T / (d_head ** 0.5)
         m = s.max()
         p = torch.exp(s - m)
         o = p @ v / p.sum()
         return o
 
+    def ref_impl_torch(q, k, v):
+        return torch.nn.functional.scaled_dot_product_attention(q[None, :], k, v)
+
     ref_out = ref_impl(q, k, v)
+    ref_out_torch = ref_impl_torch(q, k, v)
 
     BLOCK = 32
     n_blocks = seqlen // BLOCK
@@ -133,7 +137,7 @@ def test_single_q_vector():
     for i in range(n_blocks):
         k_block = k[i * BLOCK : (i + 1) * BLOCK]
         v_block = v[i * BLOCK : (i + 1) * BLOCK]
-        s = q @ k_block.T
+        s = q @ k_block.T / (d_head ** 0.5)
         m_i = torch.max(s.max(), m)
         if i == 0:
             acc = torch.exp(s - m_i) @ v_block
@@ -145,22 +149,31 @@ def test_single_q_vector():
             l += torch.exp(s - m_i).sum()
         m = m_i
     acc /= l
+
+    diff = torch.abs(acc - ref_out)
+    rel = diff / torch.abs(ref_out)
+    print(f"acc <> ref_out: {diff.max()}, {rel.max()}")
+    diff = torch.abs(ref_out - ref_out_torch)
+    rel = diff / torch.abs(ref_out_torch)
+    print(f"ref_out <> ref_out_torch: {diff.max()}, {rel.max()}")
     assert torch.allclose(acc, ref_out, atol=1e-5)
     print("test_single_q_vector passed")
 
 
 def test_full_attention():
-    seqlen = 1024
+    q_len = 1024
+    kv_len = 1024
     d_head = 8
-    q = torch.rand(seqlen, d_head)
-    k = torch.rand(seqlen, d_head)
-    v = torch.rand(seqlen, d_head)
+    q = torch.randn(q_len, d_head)
+    k = torch.randn(kv_len, d_head)
+    v = torch.randn(kv_len, d_head)
 
     def ref_impl(q, k, v):
-        s = q @ k.T
-        m = s.max(dim=1).values
-        p = torch.exp(s - m)
+        d_head = q.shape[1]
+        s = q @ k.T / (d_head ** 0.5)
+        p = torch.exp(s - s.max(dim=-1).values[:, None])
         o = p @ v / p.sum(dim=1)[:, None]
+        # p = torch.nn.functional.softmax(s, dim=1)
         return o
 
     ref_out = ref_impl(q, k, v)
@@ -170,30 +183,32 @@ def test_full_attention():
 
     torch_out = torch_impl(q, k, v)
 
-    BLOCK = 32
-    n_blocks = seqlen // BLOCK
-    out = torch.zeros_like(ref_out)
-    for i in range(n_blocks):
-        q_block = q[i * BLOCK : (i + 1) * BLOCK]
+    Q_BLOCK = 32
+    KV_BLOCK = 32
+    n_q_blocks = q_len // Q_BLOCK
+    n_kv_blocks = kv_len // KV_BLOCK
+    out = torch.zeros(q_len, d_head)
+    for i in range(n_q_blocks):
+        q_block = q[i * Q_BLOCK : (i + 1) * Q_BLOCK] # [Q_BLOCK, d_head]
         acc = None
-        m_i = torch.full((BLOCK,), float("-inf"))
-        l = torch.full((BLOCK,), float("-inf"))
-        for j in range(n_blocks):
-            k_block = k[j * BLOCK : (j + 1) * BLOCK]
-            v_block = v[j * BLOCK : (j + 1) * BLOCK]
-            s = q_block @ k_block.T
-            m_ij = torch.max(s.max(dim=1).values, m_i)
+        m_i = torch.full((Q_BLOCK,), float("-inf")) # [Q_BLOCK]
+        l = torch.full((Q_BLOCK,), float("-inf")) # [Q_BLOCK]
+        for j in range(n_kv_blocks):
+            k_block = k[j * KV_BLOCK : (j + 1) * KV_BLOCK] # [KV_BLOCK, d_head]
+            v_block = v[j * KV_BLOCK : (j + 1) * KV_BLOCK] # [KV_BLOCK, d_head]
+            s = q_block @ k_block.T / (d_head ** 0.5) # [Q_BLOCK, KV_BLOCK]
+            m_ij = torch.max(s.max(dim=-1).values, m_i) # [Q_BLOCK]
             if j == 0:
-                acc = torch.exp(s - m_ij) @ v_block
-                l = torch.exp(s - m_ij).sum(dim=1)
+                acc = torch.exp(s - m_ij[:, None]) @ v_block # [Q_BLOCK, d_head]
+                l = torch.exp(s - m_ij[:, None]).sum(dim=-1) # [Q_BLOCK]
             else:
-                acc *= torch.exp(m_i - m_ij)[:, None]
-                acc += torch.exp(s - m_ij) @ v_block
-                l *= torch.exp(m_i - m_ij)
-                l += torch.exp(s - m_ij).sum(dim=1)
-            m_i = m_ij
-        acc /= l[:, None]
-        out[i * BLOCK : (i + 1) * BLOCK] = acc
+                acc *= torch.exp(m_i - m_ij)[:, None] # [Q_BLOCK, d_head]
+                acc += torch.exp(s - m_ij[:, None]) @ v_block # [Q_BLOCK, d_head]
+                l *= torch.exp(m_i - m_ij) # [Q_BLOCK]
+                l += torch.exp(s - m_ij[:, None]).sum(dim=-1) # [Q_BLOCK]
+            m_i = m_ij # [Q_BLOCK]
+        acc /= l[:, None] # [Q_BLOCK, d_head]
+        out[i * Q_BLOCK : (i + 1) * Q_BLOCK] = acc
     diff = torch.abs(out - ref_out)
     rel = diff / torch.abs(ref_out)
     print(f"out <> ref_out: {diff.max()}, {rel.max()}")
@@ -201,18 +216,6 @@ def test_full_attention():
     rel = diff / torch.abs(torch_out)
     print(f"ref_out <> torch_out: {diff.max()}, {rel.max()}")
 
-    assert torch.allclose(out, ref_out, atol=3e-2, rtol=1e-2)
+    assert torch.allclose(out, ref_out, atol=1e-5)
     print("test_full_attention passed")
 
-
-def main():
-    # test_dot_attention_order()
-    # test_accumuate_denominator()
-    # test_accumuate_numerator()
-    # test_single_q_number()
-    # test_single_q_vector()
-    test_full_attention()
-
-
-if __name__ == "__main__":
-    main()
