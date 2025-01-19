014957753e2a0ed6a4a1e21709350aa9e9113fe0
Hairong Mu <hairongmu@MacBookAir.attlocal.net>
2025-01-18 17:19:08 -0800

    Seperate post and feed services.

--- newsfeed/backend/feed_service.py
+++ newsfeed/backend/feed_service.py
@@ -1,1 +1,41 @@
-# TODO (hairong) implement this
+from fastapi import FastAPI
+import uvicorn
+from config import SERVICE_HOST, FEED_SERVICE_PORT
+from utils import create_users, validate_users
+from post_service import Post, PostService
+
+
+class FeedService:
+    def __init__(self, users: dict[str, set[str]]) -> None:
+        self.users = users
+
+    def feed(self, uid: str, start_ts: float, end_ts: float) -> list[Post]:
+        # Get the list of users that the user follows
+        assert uid in self.users, f"User {uid} does not exist"
+        friends = self.users.get(uid, set())
+        posts = PostService.get_users_posts(friends + uid, start_ts, end_ts)
+        posts.sort(key=lambda post: post.timestamp)
+        return posts
+
+
+def feed_service_handler(feed_service: FeedService) -> FastAPI:
+    app = FastAPI()
+
+    @app.get("/feed")
+    def feed(user: str, start_ts: float, end_ts: float):
+        print(f"Getting feed for user {user} between {start_ts} and {end_ts}")
+        return feed_service.feed(user, start_ts, end_ts)
+
+    return app
+
+
+def create_feed_service() -> None:
+    users = create_users()
+    validate_users(users)
+
+    print(f"Starting feed service on {SERVICE_HOST}:{FEED_SERVICE_PORT}")
+    uvicorn.run(feed_service_handler(users), host=SERVICE_HOST, port=FEED_SERVICE_PORT)
+
+
+if __name__ == "__main__":
+    create_feed_service()
--- newsfeed/backend/post_service.py
+++ newsfeed/backend/post_service.py
@@ -1,15 +1,45 @@
 from fastapi import FastAPI
+import time
+from collections import defaultdict
+from dataclasses import dataclass
+import uvicorn
+from config import SERVICE_HOST, POST_SERVICE_PORT
+
+
+@dataclass
+class Post:
+    uid: str
+    post_id: str
+    content: str
+    timestamp: float
 
 
 class PostService:
     def __init__(self) -> None:
-        pass
+        self.user_posts: dict[str, list[str]] = defaultdict(list)
+        self.posts: dict[str, Post] = {}
 
     def post(self, uid: str, message: str) -> bool:
-        pass
+        timestamp = float(time.time())
+        post_id = f"{uid}-{timestamp}"
+        post = Post(uid, post_id, message, timestamp)
+        self.posts[post_id] = post
+        self.user_posts[uid].append(post_id)
+        return True
 
-    def get_post_ids(self, uid: str, start_ts: float, end_ts: float) -> list[str]:
-        pass
+    def get_users_posts(
+        self, uids: list[str], start_ts: float, end_ts: float
+    ) -> list[Post]:
+        post_ids = []
+        for uid in uids:
+            post_ids += [
+                post_id
+                for post_id in self.user_posts[uid]
+                if start_ts <= self.posts[post_id].timestamp <= end_ts
+            ]
+
+        feed = [self.posts[post_id] for post_id in post_ids]
+        return feed
 
 
 def post_service_handler(post_service: PostService) -> FastAPI:
@@ -20,10 +50,18 @@ def post_service_handler(post_service: PostService) -> FastAPI:
         print(f"User {user} is posting {content}")
         return post_service.post(user, content)
 
-    # TODO (hairong) implement the get_post_ids endpoint
+    @app.get("/get_users_posts")
+    def get_users_posts(uids: list[str], start_ts: float, end_ts: float):
+        print(f"Getting posts for users {uids} between {start_ts} and {end_ts}")
+        return post_service.get_users_posts(uids, start_ts, end_ts)
 
     return app
 
-# TODO (hairong) implement the create_post_service function
 
-# TODO (hairong) implement the main function
+def create_post_service() -> None:
+    print(f"Starting post service on {SERVICE_HOST}:{POST_SERVICE_PORT}")
+    uvicorn.run(post_service_handler(), host=SERVICE_HOST, port=POST_SERVICE_PORT)
+
+
+if __name__ == "__main__":
+    create_post_service()
